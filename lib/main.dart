d6884c4d7a0b3a2e60b18384819cf1c1e0768741
jamescardona11 <jjcardonao@gmail.com>
2022-04-27 19:36:36 -0500

    change name of example to  app_bar_animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tools_tkmonkey/tools_tkmonkey.dart';
-
-import 'controller.dart';
 
 void main() => runApp(
       ChangeNotifierProvider(
